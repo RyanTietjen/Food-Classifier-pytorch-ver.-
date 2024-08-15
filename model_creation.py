6747b80351bb9f8cc0720c3e743f55bf34534637
RyanTietjen <36613081+RyanTietjen@users.noreply.github.com>
2024-08-15 12:57:01 +0000

    updating effnetb2 creation

--- model_creation.py
+++ model_creation.py
@@ -8,6 +8,17 @@ import torchvision
 import torch
 from torch import nn
 
+def create_model(config,
+                 num_classes:int=101, 
+                 seed:int=31, 
+                 freeze_gradients:bool=True):
+    if config["Model Utilization"]["model_type"] == "effnetb2":
+        return effnetb2(num_classes=num_classes,
+                        seed=seed,
+                        freeze_gradients=freeze_gradients)
+    else:
+        raise Exception("Invalid config provided")
+
 def create_linear_model():
     """
     TODO: Implement
@@ -22,18 +33,21 @@ def ViT():
 
 
 def effnetb2(num_classes:int=101, 
-                          seed:int=42,
-                          freeze_gradients:bool=True):
-    
+             seed:int=31, 
+             freeze_gradients:bool=True):
     torch.manual_seed(seed)
-    weights = torchvision.models.EfficientNet_B2_Weights.DEFAULT
-    transforms = weights.transforms()
-    model = torchvision.models.efficientnet_b2(weights)
 
+    #Create model and extract weights/transforms
+    weights = torchvision.models.EfficientNet_B2_Weights.IMAGENET1K_V1
+    transforms = weights.transforms()
+    model = torchvision.models.efficientnet_b2(weights=weights)
+
+    #Freeze gradients to avoid modifying the original model    
     if freeze_gradients:
         for param in model.parameters():
             param.requires_grad = False
 
+    #modify classifier model to fit our 
     model.classifier = nn.Sequential(
         nn.Dropout(p=0.2, inplace=True),
         nn.Linear(in_features=1408, out_features=num_classes),
